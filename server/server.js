a00cbd17e1e1fde577dc7487dffc0c4193d111ba
iso-lation <ismailyussuf011@gmail.com>
2025-04-24 11:12:24 -0700

    initial

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const express = require('express')
+const app = express()
+const port = 3000;
+app.use(express.json())
+
+app.get('/api/spells', (req, res) => {
+    const data = fs.readFileSync('spells.json', 'utf-8')
+    const spells = JSON.parse(data)
+    res.json(spells)
+})
+
+app.post('/api/spells', (req, res) => {
+    const data = fs.readFileSync('spells.json', 'utf-8')
+    const spells = JSON.parse(data)
+    const newSpell = req.body
+    spells.push(newSpell)
+    res.json(spells)        
+})
+
+app.listen(port, () => {
+    console.log('Spellbook server is running on port 3000')
+})
