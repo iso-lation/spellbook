eeea5d937e48cf6fead9dfc6f1c4b3bcaa21b9b1
iso-lation <ismailyussuf011@gmail.com>
2025-05-25 00:35:37 -0700

    style changes

--- client/src/App.js
+++ client/src/App.js
@@ -26,21 +26,43 @@ function App() {
       setSpells(data)
   })}, []
   )
+  const spellPairs = [];
+  for (let i = 0; i < spells.length; i += 2) {
+    spellPairs.push([spells[i], spells[i + 1]]);
+  }
+
   
   return (
-    <div> 
-       <HTMLFlipBook width={600} height={400}>
-        <div className="page">
-          <h2>{spells[0]?.name}</h2>
-          <p>{spells[0]?.description}</p>
+    <div>
+      {spells.length > 0 && typeof window !== "undefined" &&  (
+        <div className="App">  
+          <HTMLFlipBook width={800} height={850} size="stretch" minWidth={600} maxWidth={1000} minHeight={400}
+            maxHeight={800} drawShadow={true} flippingTime={600} useMouseEvents={true} className="spellbook"
+            clickEventForward={false} mobileScrollSupport={false} showCover={true}>
+            <div className="page">
+              <h1>Spellbook of Ancient Magic</h1>
+              <p>Property of Hogwarts School of Witchcraft and Wizardy</p>
+            </div>
+            
+            {spellPairs.map((pair, index) => (
+              <div className="page" key={index}>
+                {pair.map((spell, i) => (
+                  spell && (
+                    <div key={i} className="spell-entry">
+                      <h1 className="spell-title">{spell.name}</h1>
+                      <p className="spell-effect"><em>Effect:</em> {spell.effect}</p>
+                      <p className="spell-description">"{spell.description}"</p>
+                      <p className="spell-level">Level: {spell.level}</p>
+                    </div>
+                  )
+                ))}
+  </div>
+))}
+
+          </HTMLFlipBook>
         </div>
-        <div className="page">
-          <h2>{spells[1]?.name}</h2>
-          <p>{spells[1]?.description}</p>
+          )}
         </div>
-      </HTMLFlipBook>
-      
-    </div>
   );
   
 }
